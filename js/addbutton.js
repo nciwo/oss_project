f4d442c0955fd90513872437656895887ec6ed1d
nciwo <thebluesky771@gmail.com>
2024-06-04 15:13:45 +0900

    third commit

--- js/addbutton.js
+++ js/addbutton.js
@@ -1,2 +1,23 @@
 btnAdd = document.querySelector('.btnAdd');
+new_post = document.querySelector('.guest_book_new_post');
 
+console.log("off");
+
+btnAdd.addEventListener("click", () => {
+    console.log("off");
+    if(new_post.classList.contains("on")) {
+        new_post.classList.remove("on");
+        new_post.classList.add("off");
+        console.log("off");
+        return;
+    }
+
+    if(new_post.classList.contains("off")) {
+        new_post.classList.remove("off");
+        new_post.classList.add("on");
+        console.log("on");
+        return;
+    }
+    // new_post.classList.toggle("on");
+    // console.log("on");
+});
